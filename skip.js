a2228db7190dea5a6a53c080f8926dda0ec3e1bb
Tapas Kapadia <tapaskapadia10@gmail.com>
2020-12-05 00:28:23 -0600

    fixing skipping intro for latest netflix change

--- skip.js
+++ skip.js
@@ -1,13 +1,19 @@
+//parent element of what is targeted for the keyboard events
+let parentClass = '.watch-video--player-view'
+let skipButtonClass = '.watch-video--skip-content-button'
+//Key down and Key up event for the character "s" as this skips intro when available
+let key_down_event = new KeyboardEvent('keydown',{"altKey": false,"bubbles": true,"cancelBubble": false,"cancelable": true,"charCode": 0,"code": "KeyS","composed": true,"ctrlKey": false,"currentTarget": null,"defaultPrevented": true,"detail": 0,"eventPhase": 0,"isComposing": false,"isTrusted": true,"key": "s","keyCode": 83,"location": 0,"metaKey": false,"repeat": false,"returnValue": false,"shiftKey": false,"type": "keydown","which": 83})
+let key_up_event = new KeyboardEvent('keyup',{"altKey": false,"bubbles": true,"cancelBubble": false,"cancelable": true,"charCode": 0,"code": "KeyS","composed": true,"ctrlKey": false,"currentTarget": null,"defaultPrevented": false,"detail": 0,"eventPhase": 0,"isComposing": false,"isTrusted": true,"key": "s","keyCode": 83,"location": 0,"metaKey": false,"repeat": false,"returnValue": true,"shiftKey": false,"type": "keyup","which": 83})
 checkSite = n => {
     chrome.storage.sync.get('onOffTesting',function(result){
         if(result.onOffTesting !== "off") {
-            let matchClass = '.skipElement'
-            if(window.location.origin.includes('netflix')) {
-                matchClass = '.skip-credits'
-            } 
-            if (n.nodeType === 1 && n.matches(matchClass)) {
-                skip(n,matchClass)
-            }	
+            //don't need if only support netflix
+            // if(window.location.origin.includes('netflix')) {
+            //     matchClass = '.watch-video--skip-content-button'
+            // } 
+            if (n.nodeType === 1 && n.querySelector(skipButtonClass)) {
+                skip(parentClass)
+            }
         }
     })
 	
@@ -15,19 +21,12 @@ checkSite = n => {
 
 checkAddedNodes = n => n.addedNodes.forEach(checkSite);
 
-skip = (n, m) => {
-    if (m === '.skip-credits') {
-        doClick(n)
-    } else {
-        n.click();
-    }
+skip = (pc) => {
+    let ele = document.querySelector(pc).firstChild;
+    ele.dispatchEvent(key_down_event);
+    ele.dispatchEvent(key_up_event);
 }
 
-function doClick(n) {
-    return new Promise(function(resolve) {
-        resolve(n.firstChild.click());
-    });
-}
 
 const obs = new MutationObserver(m => m.map(checkAddedNodes));
 
